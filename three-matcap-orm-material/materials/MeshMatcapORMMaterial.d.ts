0cf729e11734a736e413caab64de49d886da075e
Sébastien VINCENT <svincent@wanadev.fr>
2023-08-21 12:49:55 +0200

    up with THREE

--- three-matcap-orm-material/materials/MeshMatcapORMMaterial.d.ts
+++ three-matcap-orm-material/materials/MeshMatcapORMMaterial.d.ts
@@ -1,15 +1,15 @@
-import { Color, MeshMatcapMaterial, Texture, type MeshMatcapMaterialParameters } from 'three';
-export declare class MeshMatcapORMMaterial extends MeshMatcapMaterial {
+import * as THREE from 'three';
+export declare class MeshMatcapORMMaterial extends THREE.MeshMatcapMaterial {
     private customUniforms;
-    constructor(parameters?: MeshMatcapMaterialParameters);
-    set color2(value: Color);
-    get color2(): Color;
-    set map2(value: Texture | null);
-    get map2(): Texture | null;
+    constructor(parameters?: THREE.MeshMatcapMaterialParameters);
+    set color2(value: THREE.Color);
+    get color2(): THREE.Color;
+    set map2(value: THREE.Texture | null);
+    get map2(): THREE.Texture | null;
     set roughness(value: number);
     get roughness(): number;
-    set roughnessMap(value: Texture | null);
-    get roughnessMap(): Texture | null;
+    set roughnessMap(value: THREE.Texture | null);
+    get roughnessMap(): THREE.Texture | null;
     set metalness(value: number);
     get metalness(): number;
 }
